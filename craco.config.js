b6d50f643532ba9a4003acf30b0518ba45862c88
Kyle Scott <kyscott@umich.edu>
2022-02-24 02:03:04 -0500

    files emitted

--- craco.config.js
+++ craco.config.js
@@ -3,6 +3,14 @@ const SentryWebpackPlugin = require('@sentry/webpack-plugin')
 const now = Math.floor(new Date().getTime() / 1000)
 
 module.exports = {
+  babel: {
+    presets: [
+      [
+        "@babel/preset-react",
+        { runtime: "automatic", importSource: "@emotion/react" },
+      ],
+    ],
+  },
   webpack: {
     plugins: {
       // add: process.env.SENTRY_AUTH_TOKEN
