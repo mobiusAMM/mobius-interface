d28f6c9997bd2f6f9f0def52e167972cf728127d
Kyle Scott <kyscott@umich.edu>
2022-02-26 22:40:56 -0500

    .

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,23 @@
-// eslint-disable-next-line no-undef
-module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    ecmaFeatures: {
-      // Allows for the parsing of JSX
-      jsx: true,
-    },
-  },
-  ignorePatterns: ['node_modules/**/*', 'craco.config.js'],
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
-  plugins: ['simple-import-sort', 'import', 'unused-imports'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
-  ],
-  rules: {
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/camelcase': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
+'use strict'
 
-    'import/order': 'off',
-    'simple-import-sort/imports': 'warn',
-    'simple-import-sort/exports': 'warn',
-    'import/first': 'warn',
-    'import/newline-after-import': 'warn',
-    'import/no-duplicates': 'warn',
-    'unused-imports/no-unused-imports': 'error',
+// Required in Yarn 2 (PNP)
+// https://github.com/yarnpkg/berry/issues/8#issuecomment-681069562
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['*.js'],
+  extends: ['@saberhq/eslint-config-react', 'plugin:jsx-a11y/recommended'],
+  parserOptions: {
+    project: 'tsconfig.json',
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/no-unescaped-entities': 'off',
   },
 }
--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,22 @@
-const SentryWebpackPlugin = require('@sentry/webpack-plugin')
 
-const now = Math.floor(new Date().getTime() / 1000)
+const fs = require("fs");
+const path = require("path");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const webpack = require("webpack");
+
+const srcDirs = fs.readdirSync(path.resolve(__dirname, "./src"), {
+  withFileTypes: true,
+});
+
+const aliases = srcDirs
+  .filter((dir) => dir.isDirectory())
+  .reduce(
+    (acc, el) => ({
+      ...acc,
+      [el.name]: path.resolve(__dirname, `./src/${el.name}`),
+    }),
+    {}
+  );
 
 module.exports = {
   babel: {
@@ -10,13 +26,101 @@ module.exports = {
         { runtime: "automatic", importSource: "@emotion/react" },
       ],
     ],
+    plugins: [
+      "@emotion/babel-plugin",
+      "babel-plugin-twin",
+      "babel-plugin-macros",
+      [
+        "@simbathesailor/babel-plugin-use-what-changed",
+        {
+          active: process.env.NODE_ENV === "development", // boolean
+        },
+      ],
+    ],
   },
   webpack: {
-    plugins: {
+    alias: aliases,
+    configure: (config) => {
+      const htmlWebpackPlugin = config.plugins.find(
+        (plugin) => plugin.constructor.name === "HtmlWebpackPlugin"
+      );
+      if (!htmlWebpackPlugin) {
+        throw new Error("Can't find HtmlWebpackPlugin");
+      }
+
+      config.resolve.fallback = {
+        string_decoder: require.resolve("string_decoder/"),
+        stream: require.resolve("stream-browserify"),
+        path: false,
+        fs: false,
+        util: false,
+      };
+
+      config.plugins.unshift(
+        new webpack.ProvidePlugin({
+          Buffer: ["buffer", "Buffer"],
+          process: "process/browser",
+        })
+      );
+
+      config.module.rules.push({
+        test: /\.m?js/,
+        resolve: {
+          fullySpecified: false,
+        },
+      });
+
+
+      // htmlWebpackPlugin.userOptions = deepMerge(htmlWebpackPlugin.userOptions, {
+      //   title: appInfo.title ?? appInfo.name,
+      //   meta: {
+      //     viewport: "width=device-width, initial-scale=1",
+      //     description: appInfo.description,
+
+      //     "og:title": appInfo.name,
+      //     "og:site_name": appInfo.name,
+      //     "og:url": appInfo.url,
+      //     "og:description": appInfo.description,
+      //     "og:type": "website",
+      //     "og:image": appInfo.image,
+
+      //     "twitter:card": "summary_large_image",
+      //     "twitter:site": `@${appInfo.socials.twitter}`,
+      //     "twitter:url": appInfo.url,
+      //     "twitter:title": appInfo.name,
+      //     "twitter:description": appInfo.description,
+      //     "twitter:image": appInfo.image,
+      //     "twitter:image:alt": appInfo.imageAlt,
+      //   },
+      // });
+
+      // pushing here ensures that the dotenv is loaded
+      if (process.env.ANALYZE) {
+        config.plugins.push(
+          new BundleAnalyzerPlugin({ analyzerMode: "server" })
+        );
+      }
+
+      // config.plugins.push(
+      //   new FaviconsWebpackPlugin({
+      //     logo: "./public/images/icon.png",
+      //     publicPath: "/",
+      //     favicons: {
+      //       appName: appInfo.name,
+      //       appShortName: appInfo.name,
+      //       appDescription: appInfo.description,
+      //       developerName: `${appInfo.name} Team`,
+      //       developerURL: appInfo.url,
+      //       theme_color: appInfo.themeColor,
+      //     },
+      //   })
+      // );
+
+      return config;
     },
   },
   eslint: {
     enable: false,
   },
   typescript: { enableTypeChecking: false },
-}
+};
